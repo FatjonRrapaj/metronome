9fe2b18d61d23906316169dcb5ef9db6411a6261
Fatjon Rrapaj <38084457+FatjonRrapaj@users.noreply.github.com>
2022-04-25 12:47:41 +0200

    nois

--- src/Scene/index.js
+++ src/Scene/index.js
@@ -1,21 +1,78 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { DoubleSide } from "three";
+import SimplexNoise from "simplex-noise";
+import noiseShader from "./noiseShader";
 
 function Sphere() {
   const sphere = useRef();
+  const geometry = useRef();
+  const noise = new SimplexNoise();
+
+  useEffect(() => {
+    console.log("GEOMETRY", geometry.current);
+  }, [geometry.current]);
 
   useFrame(() => {
-    if (sphere.current) {
-      sphere.current.rotation.x -= 0.01;
-      sphere.current.rotation.y -= 0.01;
+    if (sphere.current && geometry.current) {
+      sphere.current.rotation.y += 0.005;
     }
   });
 
+  let materialShader = useRef(null).current;
+
   return (
     <mesh ref={sphere}>
-      <icosahedronBufferGeometry attach="geometry" args={[1, 10]} />
-      <meshBasicMaterial side={DoubleSide} color="#ff00ff" wireframe={true} />
+      <icosahedronBufferGeometry
+        ref={geometry}
+        attach="geometry"
+        args={[100, 100]}
+      />
+      <meshStandardMaterial
+        side={DoubleSide}
+        color="#ff00ff"
+        wireframe={true}
+        onBeforeCompile={(shader) => {
+          // The perlin noise code goes here, above the main() function in the shader.
+          // Noise shader from https://github.com/ashima/webgl-noise.
+          shader.vertexShader = shader.vertexShader.replace(
+            "#include <uv_pars_vertex>",
+            noiseShader
+          );
+          /**
+           * Adding controls to existing glsl.
+           */
+          shader.uniforms = {
+            ...shader.uniforms,
+            uNoiseFactor: {
+              //80.0
+              value: 80.0,
+            },
+            uPosotionNoiseFactor: {
+              //5.0
+              value: 0,
+            },
+          };
+
+          shader.vertexShader =
+            "uniform float uNoiseFactor;\n" +
+            "uniform float uPosotionNoiseFactor;\n" +
+            shader.vertexShader;
+
+          // The vertex shader code that goes inside main() needs to be separate from the perlin noise code.
+          shader.vertexShader = shader.vertexShader.replace(
+            "#include <worldpos_vertex>",
+            `vUv = uv;
+                    noise = uNoiseFactor * turbulence(normal);
+                    float b = uPosotionNoiseFactor * pnoise(0.01 * position, vec3(100.0));
+                    float displacement = noise + b;
+                    vec3 newPosition = position + normal * displacement;
+                    gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);`
+          );
+
+          materialShader = shader;
+        }}
+      />
     </mesh>
   );
 }
@@ -25,10 +82,10 @@ function Scene() {
     <Canvas
       shadows
       camera={{
-        far: 100,
+        far: 500,
         near: 1,
         fov: 45,
-        position: [0, 0, 10],
+        position: [0, 0, 450],
         aspect: window.innerWidth / window.innerHeight,
       }}
       antialias
